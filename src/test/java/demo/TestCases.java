0a632b86a803b7d45530de87b6c0cca1621c5264
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2024-09-02 23:03:12 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,27 +1,185 @@
 package demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 
 public class TestCases {
-    ChromeDriver driver;
+    WebDriver driver;
+
+    @FindBy(xpath = "//a[contains(text(),'Hockey Teams: Forms, Searching and Pagination')]")
+    private WebElement Hockeyteam;
+
+    @FindBy(xpath = "//tr[@class='team']")
+    private List<WebElement> tableRow;
+
+    @FindBy(xpath = "//ul[@class='pagination']//a[normalize-space()='1']")
+    private WebElement pagination;
+
+    @FindBy(xpath = "//ul[@class='pagination']/li/a/strong[contains(text(),'1')]")
+    private WebElement activePageOne;
+
+    @FindBy(xpath = "//a[normalize-space()='1']")
+    private WebElement Pageone;
+
+    @FindBy(xpath = "//a[@aria-label='Next']")
+    private WebElement NextPagebutton;
+
+    @FindBy(xpath = "//a[contains(text(),'Oscar Winning Films: AJAX and Javascript')]")
+    private WebElement oscarWinning;
+
+    @FindBy(xpath = "//a[@class='year-link']")
+    private List<WebElement> yearlinks;
+
+    @FindBy(xpath = "//tr[@class='film']")
+    private List<WebElement> Movierows;
+
+    @FindBy(xpath = "//th[text()='Title']")
+    private WebElement movietitleheading;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
 
+
+
+    @Test(enabled = true)
+    public void testCase01(){
+        System.out.println("Starting with testCase 01");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://www.scrapethissite.com/pages/");
+
+        Wrappers.click(driver,Hockeyteam);
+        List<HashMap<String,Object>> HockeyTeams = new ArrayList<>();
+
+        if(!Wrappers.isElementPresent(driver,activePageOne)){
+            Wrappers.click(driver,Pageone);
+        }
+        for(int i=1;i<=4;i++){
+            for(WebElement row: tableRow){
+
+                String teamName = row.findElement(By.xpath("./td[1]")).getText().trim();
+                String Year = row.findElement(By.xpath("./td[2]")).getText().trim();
+                String winpercentagestring = row.findElement(By.xpath("./td[6]")).getText().trim();
+                Double Winpercentage = Double.parseDouble(winpercentagestring);
+                long epochtime = Wrappers.getCurrentEpochTime();
+
+                if(Winpercentage<0.40){
+                    HashMap<String, Object> teamData =new HashMap<>();
+                    teamData.put("Epoch Time", epochtime);
+                    teamData.put("Team Name", teamName);
+                    teamData.put("Year",Year);
+                    teamData.put("Win Percentage",Winpercentage);
+                    HockeyTeams.add(teamData);
+                }
+
+            }
+            if(NextPagebutton.isEnabled()){
+                Wrappers.click(driver,NextPagebutton);
+            }else{
+                break;
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                mapper.writeValue(new File("hockey-team-data.json"),HockeyTeams);
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+
+
+    }
+    @Test(enabled = true)
+    public void testCase2() throws InterruptedException {
+        System.out.println("Starting with testCase 02");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://www.scrapethissite.com/pages/");
+
+        Wrappers.click(driver,oscarWinning);
+        List<HashMap<String,Object>> MovieList = new ArrayList<>();
+
+        for (WebElement yearlink : yearlinks){
+            String year = yearlink.getText().trim();
+            Wrappers.click(driver,yearlink);
+//            wait.until(ExpectedConditions.visibilityOf(movietitleheading));
+            Thread.sleep(4000);
+            for(int i=0;i<5&&i<Movierows.size();i++){
+                WebElement row= Movierows.get(i);
+                String title = row.findElement(By.xpath("./td[@class='film-title']")).getText().trim();
+                String Nomination = row.findElement(By.xpath("./td[@class='film-nominations']")).getText().trim();
+                String Awards = row.findElement(By.xpath("./td[@class='film-awards']")).getText().trim();
+                boolean isWinner = false;
+
+                try {
+                    WebElement winnerIcon = row.findElement(By.xpath("./td/i[@class='glyphicon glyphicon-flag']"));
+                    if (winnerIcon.isDisplayed()) {
+                        isWinner = true; // Set to true only for the Best Picture winner
+                    }
+                } catch (NoSuchElementException e) {
+
+                    isWinner = false;
+                }
+                long epochtime = Wrappers.getCurrentEpochTime();
+
+                HashMap<String,Object> MovieData = new HashMap<>();
+                MovieData.put("Epoch Time:", epochtime);
+                MovieData.put("Year:",year);
+                MovieData.put("Title:",title);
+                MovieData.put("Nomination:",Nomination);
+                MovieData.put("Awards:", Awards);
+                MovieData.put("isWinner:",isWinner);
+                MovieList.add(MovieData);
+
+            }
+            driver.navigate().back();
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        File outputfolder = new File("Output");
+        if(!outputfolder.exists()){
+            outputfolder.mkdir();
+        }
+        File jsonfile = new File(outputfolder,"oscar-winner-data.json");
+        try {
+            mapper.writeValue(jsonfile,MovieList);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(jsonfile.exists(), "JSON file is not present");
+        Assert.assertTrue(jsonfile.length() > 0, "JSON file is empty");
+
+
+    }
      
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
@@ -32,7 +190,7 @@ public class TestCases {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+        WebDriverManager.chromedriver().timeout(30).setup();
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
@@ -47,6 +205,7 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver,5), this);
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -4,10 +4,9 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -16,4 +15,54 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    WebDriver driver;
+
+    public static boolean click(WebDriver driver, WebElement element){
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            Actions act = new Actions(driver);
+            act.moveToElement(element);
+            Thread.sleep(5000);
+            act.click(element).perform();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            try{
+                element.click();
+                return true;
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean sendkeys(WebDriver driver, WebElement inputbox, String keystosend){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(inputbox));
+        try {
+            inputbox.sendKeys(keystosend);
+            return true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+    public static long getCurrentEpochTime() {
+        return Instant.now().getEpochSecond();
+    }
+
+    public static boolean isElementPresent(WebDriver driver, WebElement element) {
+        try {
+            return element.isDisplayed();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
 }
